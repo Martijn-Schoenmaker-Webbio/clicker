29894bc8de511867c3c7329d599b2a889f28032e
Martijn Schoenmaker Webbio <martijn@webbio.nl>
2019-01-04 10:07:39 +0100

    Start

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$( document ).ready(function() {
+  var numberHolder = $(".number");
+  
+  if(localStorage.getItem('amountOfClicks')){
+    numberHolder.html(localStorage.getItem('amountOfClicks'));
+  } else {
+    numberHolder.html(0);
+    localStorage.setItem('amountOfClicks', numberHolder.html());
+  }
+  
+  if(localStorage.getItem('clickGrowth')){
+  } else {
+    localStorage.setItem('clickGrowth', 1)
+  }
+  
+  if(localStorage.getItem('pointerAmount')){
+    var pointerAmount = localStorage.getItem('pointerAmount');
+  } else {
+    localStorage.setItem('pointerAmount', 0)
+  }
+    
+  function addCookies(amount){
+    numberHolder.html(Number(numberHolder.html()) + Number(amount));
+    localStorage.setItem('amountOfClicks', numberHolder.html());
+  }
+  
+  $( ".number" ).click(function() {
+    clickAnimation(numberHolder, 80);
+    addCookies(localStorage.getItem('clickGrowth'));
+  });
+  
+  $( ".pointer" ).click(function() {
+    addPointer();
+  });
+      var test = 0;
+  function addPointer(){
+    localStorage.setItem('pointerAmount', (Number(localStorage.getItem('pointerAmount')) + 1));
+    clearInterval(pointerInterval);
+    var pointerInterval = setInterval( function() { 
+      addCookies(localStorage.getItem('clickGrowth')); 
+    }, (2000 / localStorage.getItem('pointerAmount')) );
+  }
+  
+  function clickAnimation(target, ms){
+    target.css('animation','' + ms + 'ms pulsate ease-in-out');
+    setTimeout(() => { finishedClick(target) }, ms);
+  }
+  
+  function finishedClick(target){
+    target.css('animation','none');
+  }
+});
